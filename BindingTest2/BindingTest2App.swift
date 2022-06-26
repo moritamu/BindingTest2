a6910713e2cb76c8dc5b83285c2ee808b27c63bc
moritamu <moris@r.sannet.ne.jp>
2022-06-26 17:24:46 +0900

    Initial Commit

---
+++ BindingTest2/BindingTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  BindingTest2App.swift
+//  BindingTest2
+//
+//  Created by 森田晋 on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BindingTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
